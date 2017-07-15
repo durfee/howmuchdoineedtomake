27430c18fca4d33115eff58562e817e867602a54
Sean Durfee <durfee.sean@gmail.com>
2017-07-15 20:13:42 +0100

    add normalize

--- src/index.js
+++ src/index.js
@@ -3,5 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+const normalize = require('normalize.css');
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
